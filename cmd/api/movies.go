c92241bf2e14eabedf80f07fd4cb116dcbfd50ee
Maksym Dombrov <maksymdombrov@gmail.com>
2026-06-02 21:18:54 +0300

    init

---
+++ cmd/api/movies.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/mdombrov-33/greenlight/internal/data"
+	"github.com/mdombrov-33/greenlight/internal/validator"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+	}
+
+	// Copy the values from the input struct into a new Movie struct, rather than
+	// decoding directly into a Movie. Decoding straight into a Movie would let a
+	// client set fields like ID and Version via the JSON request, which we don't
+	// want. Using an intermediary input struct avoids that and keeps things clean.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+
+	if err != nil {
+		app.notFoundResponse(w, r)
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mdombrov-33/greenlight
+
+go 1.26.3
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
